5c9a17c441d6f53d9a29def6b8113dcdfecba386
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-16 10:19:17 -0400

    Update install_foxtrotgps.sh

--- Software Install Scripts/install_foxtrotgps.sh
+++ Software Install Scripts/install_foxtrotgps.sh
@@ -8,28 +8,13 @@ print_info() {
     echo -e "\e[34mINFO: $1\e[0m"
 }
 
-print_info "Installing FoxtrotGPS..."
+print_info "Installing FoxtrotGPS, gpsd, and gpsd-tools..."
 
 # Update the system
 sudo apt update
 
-# Install FoxtrotGPS
-sudo apt install -y foxtrotgps
+# Install FoxtrotGPS, gpsd, and gpsd-tools together
+sudo apt install -y foxtrotgps gpsd gpsd-tools
 
-# Create Desktop Entry for FoxtrotGPS
-print_info "Creating Desktop Icon for FoxtrotGPS..."
-cat > ~/Desktop/FoxtrotGPS.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=FoxtrotGPS
-Exec=foxtrotgps
-Icon=path_to_icon.png
-Comment=Run FoxtrotGPS
-Terminal=false
-Categories=GIS;Geography;Science;
-EOL
+print_info "Installation and setup completed!"
 
-chmod +x ~/Desktop/FoxtrotGPS.desktop
-
-print_info "Installation and setup of FoxtrotGPS completed!"
