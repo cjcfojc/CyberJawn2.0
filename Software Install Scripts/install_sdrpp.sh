2f8e348e1d2099e1dba1932fe93a9190afc60a68
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-16 08:37:15 -0400

    Update install_sdrpp.sh

--- Software Install Scripts/install_sdrpp.sh
+++ Software Install Scripts/install_sdrpp.sh
@@ -10,20 +10,26 @@ print_info() {
 
 # Install dependencies for SDR++
 print_info "Installing dependencies for SDR++..."
-sudo apt-get update
-sudo apt-get install -y git cmake build-essential \
+sudo apt update
+sudo apt install -y git cmake build-essential \
                         libglfw3-dev libglew-dev \
                         libvolk2-dev libsoapysdr-dev \
                         libairspy-dev libhackrf-dev \
-                        librtlsdr-dev libbladerf-dev
+                        librtlsdr-dev libbladerf-dev \
+                        libfftw3-dev libzstd-dev \
+                        libairspyhf-dev libairspyhf1 \
+                        librtaudio-dev libiio-dev \
+                        libad9361-dev
 
 # Clone the SDR++ repository
 print_info "Cloning SDR++ repository from GitHub..."
-git clone https://github.com/AlexandreRouma/SDRPlusPlus.git
+if [ ! -d "SDRPlusPlus" ]; then
+    git clone https://github.com/AlexandreRouma/SDRPlusPlus.git
+fi
 
 # Navigate to the cloned directory and make a build directory
 cd SDRPlusPlus
-mkdir build
+mkdir -p build
 cd build
 
 # Configure and compile SDR++
@@ -34,22 +40,6 @@ make -j$(nproc)
 # Install the software
 print_info "Installing SDR++..."
 sudo make install
-
-# Create Desktop Entry for SDR++
-print_info "Creating Desktop Icon for SDR++..."
-cat > ~/Desktop/SDRPlusPlus.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=SDR++
-Exec=sdrpp
-Icon=path_to_icon.png
-Comment=Run SDR++
-Terminal=false
-Categories=HamRadio;Radio;
-EOL
-
-chmod +x ~/Desktop/SDRPlusPlus.desktop
 
 print_info "Installation and setup of SDR++ completed!"
 
