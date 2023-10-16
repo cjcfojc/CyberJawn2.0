d3054889b6204a16938d202db1c19e5d6cee174e
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-16 09:35:04 -0400

    Update install_gridtracker.sh

--- Software Install Scripts/install_gridtracker.sh
+++ Software Install Scripts/install_gridtracker.sh
@@ -8,12 +8,20 @@ print_info() {
     echo -e "\e[34mINFO: $1\e[0m"
 }
 
+# Ensure dependencies
+print_info "Updating package list..."
+sudo apt update > /dev/null
+print_info "Ensuring wget is installed..."
+sudo apt install -y wget
+
 # Download and install NWJS
 print_info "Downloading and installing NWJS..."
-wget -O nwjs-downloader.deb "https://gridtracker.org/index.php/downloads/debian-nwjs-downloader/routedownload/nwjs-downloader-0-68-1"
+wget -O nwjs-downloader.deb "https://raw.githubusercontent.com/cjcfojc/CyberJawn2.0/main/nwjs-downloader_0.68.1-1_all.deb"
 sudo apt install ./nwjs-downloader.deb
-sudo apt-get install -f
-nwjs-downloader --install
+sudo apt install -f
+
+# Clean up the nwjs-downloader.deb file
+rm nwjs-downloader.deb
 
 # Download and extract GridTracker
 print_info "Downloading and installing GridTracker..."
@@ -21,24 +29,11 @@ wget -O GridTracker.tar.gz "https://downloads.gridtracker.org/v1.23.1015/GridTra
 mkdir -p GridTracker
 tar -xf GridTracker.tar.gz -C GridTracker
 
+# Clean up the GridTracker.tar.gz file
+rm GridTracker.tar.gz
+
 # Provide execute permissions to the GridTracker binary
 chmod +x GridTracker/GridTracker*
-
-# Create Desktop Entry for GridTracker
-print_info "Creating Desktop Icon for GridTracker..."
-cat > ~/Desktop/GridTracker.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=GridTracker
-Exec=$(pwd)/GridTracker/GridTracker
-Icon=$(pwd)/GridTracker/path_to_icon.png
-Comment=Run GridTracker
-Terminal=false
-Categories=HamRadio;
-EOL
-
-chmod +x ~/Desktop/GridTracker.desktop
 
 print_info "GridTracker and its NWJS dependency are installed!"
 print_info "To run GridTracker, navigate to the GridTracker directory and execute the GridTracker binary."
