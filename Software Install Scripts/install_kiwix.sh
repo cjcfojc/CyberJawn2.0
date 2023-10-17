747da0c8f8f1927713bad92d88f1bc41d1b94d9e
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-16 20:33:49 -0400

    Update install_kiwix.sh

--- Software Install Scripts/install_kiwix.sh
+++ Software Install Scripts/install_kiwix.sh
@@ -6,7 +6,7 @@ sudo apt update
 
 # Install dependencies
 echo "Installing dependencies..."
-sudo apt install -y git cmake build-essential qtbase5-dev qttools5-dev-tools qtwebengine5-dev libqt5webenginecore5 libqt5webenginewidgets5 libqt5webchannel5-dev qtdeclarative5-dev libqt5gui5 libqt5widgets5 libqt5network5 libqt5qml5 libqt5core5a zlib1g-dev liblzma-dev libicu-dev libxapian-dev libzim-dev
+sudo apt install -y git cmake build-essential qtbase5-dev qttools5-dev-tools qtwebengine5-dev libqt5webenginecore5 libqt5webenginewidgets5 libqt5webchannel5-dev qtdeclarative5-dev libqt5gui5 libqt5widgets5 libqt5network5 libqt5qml5 libqt5core5a zlib1g-dev liblzma-dev libicu-dev libxapian-dev libzim-dev libkiwix-dev
 
 # Clone the kiwix-desktop repository
 echo "Cloning kiwix-desktop repository..."
