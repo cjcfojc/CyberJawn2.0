81b81a5855ff147af9ba59e68adceb475547dc5a
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-21 18:49:45 -0400

    Update install_artemis.sh

--- Software Install Scripts/install_artemis.sh
+++ Software Install Scripts/install_artemis.sh
@@ -1,55 +1,46 @@
 #!/bin/bash
 
-# Define error handling function
-function handle_error {
-    echo "Error occurred on line $1."
-    exit 1
-}
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
-# Set trap for error handling
-trap 'handle_error $LINENO' ERR
+# Update and upgrade system packages
+apt-get update
+apt-get upgrade -y
 
-# Update and upgrade the system
-sudo apt-get update && sudo apt-get upgrade -y
+# Install dependencies
+apt-get install -y \
+    git \
+    python3 \
+    python3-pip \
+    libqt5widgets5 \
+    libqt5gui5 \
+    libqt5core5a \
+    qt5-default \
+    pyqt5-dev \
+    pyqt5-dev-tools \
+    python3-pyqt5 \
+    python3-pyqt5.qtsvg \
+    libssl-dev \
+    openssl \
+    python3-setuptools \
+    python3-tk
 
-# Installing essential tools
-sudo apt-get install -y git
+# Clone Artemis repository from your fork
+git clone https://github.com/cjcfojc/Artemis.git
 
-# Clone Artemis repository
-git clone https://github.com/AresValley/Artemis.git
-
-# Change to the Artemis directory
+# Change directory to Artemis
 cd Artemis
 
-# Change to the spec_files/Linux directory
-cd spec_files/Linux
-
-# Installing dependencies and setting up the environment
-sudo apt-get update
-sudo apt-get install -y build-essential python3 python3-pip python3-dev python3-venv python3-setuptools zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git libopenjp2-7-dev libssl-dev libharfbuzz0b libfreetype6 libgstreamer-plugins-base0.10-0 libgstreamer-plugins-good0.10-0 libpulse0
-sudo apt-get install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0
-sudo apt-get install -y python3-tk
-
-# Setting alias for Python commands
-echo "alias python3=python" >> ~/.bash_aliases
-echo "alias pip3=pip" >> ~/.bash_aliases
-echo "alias python-config=python3-config" >> ~/.bash_aliases
-
-# Reload the bash configuration to apply aliases
-source ~/.bashrc
-
-# Install PyInstaller
-pip3 install pyinstaller
-
-# Install Artemis requirements
+# Install Python requirements
 pip3 install --break-system-packages -r requirements/requirements.txt
 
+# Run the provided build script for Raspbian
+chmod +x spec_files/Linux/raspbian_build.sh
+./spec_files/Linux/raspbian_build.sh
 
-# Build with PyInstaller
-pyinstaller --onefile Artemis_onedir.spec
-
-# Display success message
-echo "Artemis installation and setup completed successfully."
-
-exit 0
+# Print statement on how to launch Artemis
+echo "To launch Artemis, run the command: ./path-to-artemis-executable" # Replace 'path-to-artemis-executable' with the actual path if known.
 
