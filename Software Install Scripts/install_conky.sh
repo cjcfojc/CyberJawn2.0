f6e32edf1dd69f698a228a8aa8995335419a8999
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-10-21 20:14:02 -0400

    Update install_conky.sh
    added the installation and configuration of picom compositon manager to allow for "transparency" in conky.  still testing picom and script

--- Software Install Scripts/install_conky.sh
+++ Software Install Scripts/install_conky.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Install Conky
+# Install Conky and picom
 sudo apt update
-sudo apt install -y conky-all
+sudo apt install -y conky-all picom
 
 # Fetch and replace the content of the default .conkyrc with the content from GitHub
 curl -o /home/dietpi/.conkyrc https://raw.githubusercontent.com/cjcfojc/CyberJawn2.0/main/Conky/cj20conky
 
-# Create autostart directory if it doesn't exist
+# Create autostart directory if it doesn't exist for conky
 mkdir -p /home/dietpi/.config/autostart
 
 # Create Conky desktop entry for autostart
@@ -24,5 +24,15 @@ Comment[en_US]=
 Comment=
 EOL
 
-echo "Conky installation complete. Conky will start automatically after a 20-second delay on boot."
+# Setup picom configuration
+mkdir -p /home/dietpi/.config/picom
+if [ ! -f /home/dietpi/.config/picom/picom.conf ]; then
+    cp /etc/xdg/picom.conf /home/dietpi/.config/picom/picom.conf
+fi
+
+# Ensure picom starts every time you start LXDE
+mkdir -p /home/dietpi/.config/lxsession/LXDE
+echo "@picom -b" >> /home/dietpi/.config/lxsession/LXDE/autostart
+
+echo "Conky and Picom installation complete. Conky will start automatically after a 20-second delay on boot, and Picom will start in the background."
 
