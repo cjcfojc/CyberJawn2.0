9aa28b36b4e3f5a6eec79f6a8aa6176fdbfb29df
cjcfojc <126181117+cjcfojc@users.noreply.github.com>
2023-11-21 06:13:53 -0500

    Update install_meshtastic_dpos.sh
    more testing, automate PATH adjustment for Meshtastic CLI

--- Software Install Scripts/install_meshtastic_dpos.sh
+++ Software Install Scripts/install_meshtastic_dpos.sh
@@ -3,18 +3,18 @@
 # Exit on any error
 set -e
 
-# Print a message
+# Function to print messages
 print_info() {
     echo -e "\e[34mINFO: $1\e[0m"
 }
 
-# Update System
+# Update and upgrade the system
 print_info "Updating and upgrading the system..."
 sudo apt update && sudo apt upgrade -y
 
-# Install Python and PIP with break-system-packages option
+# Install Python and PIP
 print_info "Installing Python and PIP..."
-dietpi-software install 130 --break-system-packages
+sudo apt install -y python3 python3-pip
 
 # Install Git
 print_info "Installing Git..."
@@ -28,16 +28,22 @@ pip3 install --user meshtastic --break-system-packages
 print_info "Adding user to dialout group..."
 sudo usermod -a -G dialout $USER
 
-# Clone Meshtastic repository (optional, for development or advanced uses)
+# Clone Meshtastic repository (optional, if you need to work with the source code)
 print_info "Cloning Meshtastic repository..."
 git clone https://github.com/meshtastic/Meshtastic-python.git
 
-# Navigate into the Meshtastic-python directory
+# Navigate into the repository directory
 cd Meshtastic-python
 
 # Install Meshtastic-python dependencies
 print_info "Installing Meshtastic-python dependencies..."
 pip3 install -r requirements.txt --break-system-packages
+
+# Add local bin to PATH if it's not already there
+if ! grep -q '^\s*export PATH="\$HOME/.local/bin:\$PATH"' "$HOME/.bashrc"; then
+    print_info "Adding $HOME/.local/bin to PATH in .bashrc..."
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$HOME/.bashrc"
+fi
 
 # Reboot (optional, but recommended due to group changes)
 print_info "Installation completed. It's recommended to reboot the system now."
